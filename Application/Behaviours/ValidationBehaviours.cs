bd832f48034776e139b9cd6b4eaecc2db3c7b8c7
IVladiZI <vladizcc@gmail.com>
2023-03-25 19:51:11 -0400

    Add Exceptions and layer channel

---
+++ Application/Behaviours/ValidationBehaviours.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Application.Behaviours
+{
+
+    /// <summary>
+    /// With the help of installed nugget dependencies such as validation, auto mapper and mediaTR.
+    /// Here it receives a request and a response and implements the pipeline interface.
+    /// The validation class receives two types request and response and implements an interface where Irequest and Tresponse are mediaTR's own.
+    /// </summary>
+    /// <typeparam name="Trequest"> Request </typeparam>
+    /// <typeparam name="TResponse">Response </typeparam>
+    public class ValidationBehaviours<Trequest, TResponse> : IPipelineBehavior<Trequest, TResponse> where Trequest : IRequest<TResponse>
+    {
+        /// <validators>
+        ///A validator is created before entering the handle, it will validate and filter
+        ///all the content that enters the webApi through the commands or queries.
+        /// </validators>
+        private readonly IEnumerable<IValidator<Trequest>> _validators;
+
+        public ValidationBehaviours(IEnumerable<IValidator<Trequest>> validators)
+        {
+            _validators = validators;
+        }
+        /// <Handle>
+        /// Call to the channel where first if there is any validation for the interface it will be sent to validate and if not it will proceed with the execution.
+        /// </Handle>
+        /// <param name="request">Request</param>
+        /// <param name="next">Next Process</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<TResponse> Handle(Trequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            //Query if there is any type of validator for the handle channel.
+            if (_validators.Any())
+            {
+                //If it exists, it will start calling the validator by sending it the request that arrived.
+                var context = new FluentValidation.ValidationContext<Trequest>(request);
+                //The results of these context validations are available.
+                var validationResults = await Task.WhenAll(_validators.Select(x => x.ValidateAsync(context, cancellationToken)));
+                //The errors found in the validator are collected, these errors are the ones we create for
+                //the business rules we need, they are collected and we are asked to return the error list.
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+                //We query if any type of error was found and return it as a custom exception created by us.
+                if (failures.Count != 0) throw new Exceptions.ValidationException(failures);
+            }
+            return await next();
+        }
+    }
+}
---
+++ Application/Exceptions/ValidationException.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation.Results;
+
+namespace Application.Exceptions
+{
+    /// <ValidationException>
+    /// Perzonalized exceptions are created for the client where the .Net base exception is also inherited.
+    /// </ValidationException>
+    public class ValidationException : Exception
+    {
+        public List<string> Errors { get; }
+        /// <ValidationException>
+        /// Call to the channel where first if there is any validation for the interface it will be sent to validate and if not it will proceed with the execution, the errors found will be sent in the Error List. 
+        /// </ValidationException>
+        public ValidationException() : base("An error has occurred in the validation of the request")
+        {
+            Errors = new List<string>();
+        }
+        /// <ValidationException>
+        /// All errors are collected and added to the list of errors to be sent.
+        /// </ValidationException>
+        /// <param name="failures"></param>
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            foreach (var failure in failures)
+                Errors.Add(failure.ErrorMessage);
+        }
+    }
+
+}
